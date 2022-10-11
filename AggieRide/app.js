2d176986f5289003203a931b0e86ae8bd582166b
Shaurya Srivastav <108766004+Shaurya-Srivastav@users.noreply.github.com>
2022-10-11 04:01:22 -0700

    Add files via upload

---
+++ AggieRide/app.js
@@ -0,0 +1,18 @@
+$( window ).on( "load", function() {
+  mainNav();
+  $(window).scroll(function() {
+      mainNav();
+  });
+  function mainNav() {
+      var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+      if (top > 40) $('.navbar-fixed-top').stop().animate({
+          "opacity": '1',
+          "top": '0'
+      });
+      else $('.navbar-fixed-top').stop().animate({
+          "top": '-70',
+          "opacity": '0'
+      });
+
+}
+   });
